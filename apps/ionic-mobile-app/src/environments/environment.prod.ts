eccabb8ae055d3b319c2fd831add2c4549dbe609
Mahmoud <moud.elgharably@gmail.com>
2025-10-14 11:12:32 +0200

    Commerce09_ionic

--- apps/ionic-mobile-app/src/environments/environment.prod.ts
+++ apps/ionic-mobile-app/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl_: 'http://localhost:3000/api',
+  apiUrl:'https://eclair-test-web.vercel.app/api',
   useExpirationWarning: true,
   optimisticallyUpdate: false,
   ionic: true
--- apps/ionic-mobile-app/src/environments/environment.ts
+++ apps/ionic-mobile-app/src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
   //apiUrl: process.env['NX_API_URL'] || 'http://localhost:3000/api',
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl_: 'http://localhost:3000/api',
+  apiUrl:'https://eclair-test-web.vercel.app/api',
   useExpirationWarning: true,
   optimisticallyUpdate: false,
   ionic: true
